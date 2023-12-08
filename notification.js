9f834b12386df5ce7ac07c883ffe6a5307cc40aa
valentine <jasperojukwu2@gmail.com>
2023-12-08 11:26:54 -0800

    fourth commit

--- notification.js
+++ notification.js
@@ -5,4 +5,7 @@ document.getElementById('mark-all').addEventListener('click', function () {
     document.getElementById('Mark-webber').style.backgroundColor = "white"
     document.getElementById('angela-gray').style.backgroundColor = "white"
     document.getElementById('jacob-thompson').style.backgroundColor = "white"
-})
+    document.getElementById('dot1').style.display = "none"
+    document.getElementById('dot2').style.display = "none"
+    document.getElementById('dot3').style.display = "none"
+});
